eb0bcc3f98a8e6fe5cf832e26ee5d9cb124e23ef
anitakumari16022 <anitakumari90972@gmail.com>
2025-12-31 15:02:40 +0530

    print row numbers

---
+++ pattern3.java
@@ -0,0 +1,14 @@
+public class pattern3 {
+  public static void main(String[] args) {
+    int row=5;
+    int col=1;
+    for(int cr=1;cr<=row;cr++){
+      for(int cc=1;cc<=col;cc++){
+      System.out.print(cr+" ");
+      }
+      System.out.println();
+      col++;
+
+    }
+  }
+}
