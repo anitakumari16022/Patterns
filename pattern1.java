695b0293ce4390fe30ca3397a37c8f476cb921dd
anitakumari16022 <anitakumari90972@gmail.com>
2025-12-31 07:57:50 +0530

    method to print n x n star pattern

---
+++ pattern1.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public static void nForest(int n) {
+        // Write your code here
+        for(int cr=0;cr<n;cr++){
+            for(int cc=0;cc<n;cc++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
